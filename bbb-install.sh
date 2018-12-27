568c5a0f30c08261c8feaeb00774a9edcbde0709
Fred Dixon <ffdixon@gmail.com>
2018-12-27 10:55:56 -0600

    Update key for apt-get list format in 18.04

--- bbb-install.sh
+++ bbb-install.sh
@@ -815,7 +815,7 @@ install_coturn() {
   need_pkg coturn
 
   need_pkg software-properties-common 
-  need_ppa certbot-ubuntu-certbot-bionic.list ppa:certbot/certbot 75BCA694
+  need_ppa certbot-ubuntu-certbot-bionic.list ppa:certbot/certbot 7BF5
   apt-get -y install certbot
 
   certbot certonly --standalone --preferred-challenges http \
