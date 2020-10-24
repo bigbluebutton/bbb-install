ed418f730391c970b94b32cbb3a39d0702c0515f
Fred Dixon <ffdixon@gmail.com>
2020-10-24 07:53:27 -0500

    Touch /root/.rnd to avoid random number generator:RAND_load_file:Cannot open file error

--- bbb-install.sh
+++ bbb-install.sh
@@ -302,6 +302,7 @@ HERE
     echo "deb [ arch=amd64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
     rm -f /etc/apt/sources.list.d/mongodb-org-4.0.list
 
+    touch /root/.rnd
     MONGODB=mongodb-org
     install_docker		# needed for bbb-libreoffice-docker
   fi
