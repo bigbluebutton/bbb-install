9be7c20b9de6a3fadf22e74265696fc9e5ba1e4e
Brent Baccala <cosine@freesoft.org>
2021-01-20 19:23:28 -0500

    when waiting for port 443 to clear, only wait on 443 in the local address field (we don't care about 443 in the remote address field)

--- bbb-install.sh
+++ bbb-install.sh
@@ -434,8 +434,9 @@ need_x64() {
 
 wait_443() {
   echo "Waiting for port 443 to clear "
-  while netstat -antp | grep TIME_WAIT | grep -q ":443"; do sleep 1; echo -n '.'; done
-  echo 
+  # netstat fields 4 and 6 are Local Address and State
+  while netstat -ant | awk '{print $4, $6}' | grep TIME_WAIT | grep -q ":443"; do sleep 1; echo -n '.'; done
+  echo
 }
 
 get_IP() {
