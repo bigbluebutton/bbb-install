6c7f34fc329c867f143c65d55baee4dd95e04cd4
Brent Baccala <cosine@freesoft.org>
2021-02-12 17:26:33 -0500

    silence warning messages from apt-key

--- bbb-install.sh
+++ bbb-install.sh
@@ -110,6 +110,9 @@ main() {
   LETS_ENCRYPT_OPTIONS="--webroot --non-interactive"
   SOURCES_FETCHED=false
 
+  # If grep'ing for a key in "apt-key list" is dangerous, should we find a better way to do it?
+  export APT_KEY_DONT_WARN_ON_DANGEROUS_USAGE=true
+
   need_x64
 
   while builtin getopts "hs:r:c:v:e:p:m:lxgadw" opt "${@}"; do
