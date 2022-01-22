b7b38e8ac27a41060557d869cb912a19019fcda3
DecaTec <jr@decatec.de>
2022-01-22 08:30:19 +0100

    Removed ssl_ecdh_curve

--- bbb-install.sh
+++ bbb-install.sh
@@ -898,7 +898,6 @@ server {
     ssl_protocols TLSv1.2 TLSv1.3;
     ssl_ciphers ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305:DHE-RSA-AES128-GCM-SHA256:DHE-RSA-AES256-GCM-SHA384;
     ssl_prefer_server_ciphers on;
-    ssl_ecdh_curve secp521r1:secp384r1;
     ssl_dhparam /etc/nginx/ssl/dhp-4096.pem;
     
     # HSTS (comment out to enable)
