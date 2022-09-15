50cc23a9bfcdff7b08cd1a00e239184b001a0942
anmg <a.gangan@gmail.com>
2022-09-15 10:12:54 +0300

    Update bbb-install-2.5.sh
    move shared notes url config to override config with key name update

--- bbb-install-2.5.sh
+++ bbb-install-2.5.sh
@@ -894,10 +894,6 @@ HERE
     sed -i 's/String BigBlueButtonURL = "http:/String BigBlueButtonURL = "https:/g' /var/lib/$TOMCAT_USER/webapps/demo/bbb_api_conf.jsp
   fi
 
-  if [ -f /usr/share/meteor/bundle/programs/server/assets/app/config/settings.yml ]; then
-    yq w -i /usr/share/meteor/bundle/programs/server/assets/app/config/settings.yml public.note.url "https://$HOST/pad"
-  fi
-
   # Update Greenlight (if installed) to use SSL
   if [ -f ~/greenlight/.env ]; then
     if ! grep ^BIGBLUEBUTTON_ENDPOINT ~/greenlight/.env | grep -q https; then
@@ -936,7 +932,10 @@ HERE
   mkdir -p /etc/bigbluebutton/bbb-webrtc-sfu
   TARGET=/etc/bigbluebutton/bbb-webrtc-sfu/production.yml
   touch $TARGET
-
+  
+  # configure shared notes URL
+  yq w -i "$TARGET" public.pads.url "https://$HOST/pad"
+  
   # Configure mediasoup IPs, reference: https://raw.githubusercontent.com/bigbluebutton/bbb-webrtc-sfu/v2.7.2/docs/mediasoup.md
   # mediasoup IPs: WebRTC
   yq w -i "$TARGET" mediasoup.webrtc.listenIps[0].ip "0.0.0.0"
