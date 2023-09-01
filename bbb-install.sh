e8d49d95024da0eed2de080811c9803a880ec4d6
Anton Georgiev <antobinary@users.noreply.github.com>
2023-09-01 13:47:40 -0400

    2.7: Adapt to new recommended approach for installing NodeJS (port #677)

--- bbb-install.sh
+++ bbb-install.sh
@@ -295,10 +295,10 @@ main() {
       sudo rm -r /etc/apt/sources.list.d/nodesource.list
     fi
     if [ ! -f /etc/apt/sources.list.d/nodesource.list ]; then
-      curl -sL https://deb.nodesource.com/setup_18.x | sudo -E bash -
-    fi
-    if ! apt-cache madison nodejs | grep -q node_18; then
-      err "Did not detect nodejs 18.x candidate for installation"
+      sudo mkdir -p /etc/apt/keyrings
+      curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+      NODE_MAJOR=18
+      echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
     fi
 
    if [ ! -f /usr/share/keyrings/mongodb-server-6.0.gpg ]; then
