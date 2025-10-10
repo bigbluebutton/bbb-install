cf29caccf2cbcf89afc7f77655dd90a79def54d7
Gordnho <oliderafa@gmail.com>
2025-10-10 11:51:41 +0200

    ignore packages that are on hold

--- bbb-install.sh
+++ bbb-install.sh
@@ -352,7 +352,7 @@ main() {
     chmod 644 /etc/apt/keyrings/* #make apt readable for apt
   fi
 
-  apt-get update
+  apt upgrade -o Dpkg::Options::="--ignore-hold"
   apt-get -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confnew" dist-upgrade
 
   need_pkg apt-transport-https haveged
@@ -573,7 +573,7 @@ need_pkg() {
   while fuser /var/lib/dpkg/lock >/dev/null 2>&1; do echo "Sleeping for 1 second because of dpkg lock"; sleep 1; done
 
   if [ ! "$SOURCES_FETCHED" = true ]; then
-    apt-get update
+    apt upgrade -o Dpkg::Options::="--ignore-hold"
     SOURCES_FETCHED=true
   fi
 
@@ -1413,7 +1413,7 @@ install_docker() {
     echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
      $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
     chmod 644 /etc/apt/keyrings/*
-    apt-get update
+    apt upgrade -o Dpkg::Options::="--ignore-hold"
     need_pkg docker-ce docker-ce-cli containerd.io
   fi
   if ! which docker; then err "Docker did not install"; fi
@@ -1465,7 +1465,7 @@ install_ssl() {
   mkdir -p /etc/nginx/ssl
 
   if [ -z "$PROVIDED_CERTIFICATE" ]; then
-    apt-get update
+    apt upgrade -o Dpkg::Options::="--ignore-hold"
     need_pkg certbot
 
     if [[ -f "/etc/letsencrypt/live/$HOST/fullchain.pem" ]] && [[ -f "/etc/letsencrypt/renewal/$HOST.conf" ]] \
@@ -1793,7 +1793,7 @@ HERE
 
 
 install_coturn() {
-  apt-get update
+  apt upgrade -o Dpkg::Options::="--ignore-hold"
   apt-get -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confnew" dist-upgrade
 
   need_pkg software-properties-common certbot
